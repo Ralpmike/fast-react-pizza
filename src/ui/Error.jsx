48ae86f21f601c8a81ed998c8531d1a9a5dc2be1
Ralpmike <ralphmike996@gmail.com>
2025-02-10 01:33:37 +0100

    implement errorElement for error management

--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -1,12 +1,29 @@
-import { useNavigate } from 'react-router-dom';
+import { isRouteErrorResponse, useRouteError } from "react-router";
+import { useNavigate } from "react-router";
 
 function NotFound() {
   const navigate = useNavigate();
 
+  const error = useRouteError();
+  console.log(error);
+
+  // if (isRouteErrorResponse(error)) {
+  //   return (
+  //     <div>
+  //       <h1>Oops!</h1>
+  //       <p>
+  //         <i>{error.error.message}</i>
+  //       </p>
+  //       <p>{error.statusText}</p>
+  //       <button onClick={() => navigate(-1)}>&larr; Go back</button>
+  //     </div>
+  //   );
+  // }
+
   return (
     <div>
-      <h1>Something went wrong 😢</h1>
-      <p>%MESSAGE%</p>
+      <h1>Something went wrong </h1>
+      <p>{error.message || error.error?.message || error.data}</p>
       <button onClick={() => navigate(-1)}>&larr; Go back</button>
     </div>
   );
