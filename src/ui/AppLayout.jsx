02930b9cb46bc445fb184a07e2d423f38f4f7599
Ralpmike <ralphmike996@gmail.com>
2025-02-10 01:00:05 +0100

    setuo loading indicator using useNavigation hook

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,13 +1,18 @@
-import { Outlet } from "react-router";
+import { Outlet, useNavigation } from "react-router";
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
+import Loader from "./Loader";
 
 function AppLayout() {
+  const navigation = useNavigation();
+
+  const isLoading = navigation.state === "loading";
+
   return (
     <div>
+      {isLoading && <Loader />}
       <Header />
       <main>
-        <h1>Pizza Menu</h1>
         <Outlet />
       </main>
       <CartOverview />
