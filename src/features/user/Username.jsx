eb3d805c74e1734d53bbb954ab24607a40cc206c
Ralpmike <ralphmike996@gmail.com>
2025-02-11 11:44:05 +0100

    spacing borders and display

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Raphael</div>;
+  return <div className="hidden text-sm font-semibold">Raphael</div>;
 }
 
 export default Username;
