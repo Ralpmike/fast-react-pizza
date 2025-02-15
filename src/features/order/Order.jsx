2fb0a90f215ce97d5b41c133aa6f38601f9bc207
Ralpmike <ralphmike996@gmail.com>
2025-02-15 23:44:20 +0100

    feature

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,25 +1,25 @@
 // Test ID: IIDSAT
 
-import { useLoaderData } from "react-router";
-import { getOrder } from "../../services/apiRestaurant";
+import { useLoaderData } from 'react-router';
+import { getOrder } from '../../services/apiRestaurant';
 import {
   calcMinutesLeft,
   formatCurrency,
   formatDate,
-} from "../../utilities/helpers";
+} from '../../utilities/helpers';
 
 function Order() {
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const order = useLoaderData();
   console.log(order);
   const {
-    // id,
+    id,
     status,
     priority,
     priorityPrice,
     orderPrice,
     estimatedDelivery,
-    // cart,
+    cart,
   } = order;
   const deliveryIn = calcMinutesLeft(estimatedDelivery);
 
@@ -38,7 +38,7 @@ function Order() {
         <p>
           {deliveryIn >= 0
             ? `Only ${calcMinutesLeft(estimatedDelivery)} minutes left 😃`
-            : "Order should have arrived"}
+            : 'Order should have arrived'}
         </p>
         <p>(Estimated delivery: {formatDate(estimatedDelivery)})</p>
       </div>
@@ -55,9 +55,7 @@ function Order() {
 export async function loader({ params }) {
   const { orderId } = params;
   console.log(orderId);
-
   const order = await getOrder(orderId);
-
   return order;
 }
 export default Order;
