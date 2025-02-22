b207cba1bc648784397460c1ac94bf57ec895524
Ralpmike <ralphmike996@gmail.com>
2025-02-22 21:44:16 +0100

    Integrating geolocation

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -24,7 +24,15 @@ function CreateOrder() {
   const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
   const totalPrice = totalCartPrice + priorityPrice;
 
-  const username = useSelector((state) => state.user.username);
+  const {
+    username,
+    status: addressStatus,
+    position,
+    address,
+    error: errorAddress,
+  } = useSelector((state) => state.user);
+
+  const isLoadingAddress = addressStatus === 'loading';
 
   const formErrors = useActionData();
 
@@ -37,8 +45,6 @@ function CreateOrder() {
       <h2 className="mb-8 text-xl font-semibold">
         Ready to order? Let&apos;s go!
       </h2>
-
-      <button onClick={() => dispatch(fetchAddress())}>Get Location</button>
 
       {/* <Form method="POST" action="/order/new"> */}
       <Form method="POST">
@@ -65,16 +71,37 @@ function CreateOrder() {
           </div>
         </div>
 
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+        <div className="relative mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Address</label>
           <div className="grow">
             <input
+              disabled={isLoadingAddress}
               type="text"
               className="input w-full"
               name="address"
               required
+              defaultValue={address}
             />
+            {addressStatus === 'error' && (
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                {errorAddress}
+              </p>
+            )}
           </div>
+          {!position.latitude && !position.longitude && (
+            <span className="absolute top-[3px] right-[3px] z-50">
+              <Button
+                disabled={isLoadingAddress}
+                type="small"
+                onclick={(e) => {
+                  e.preventDefault();
+                  dispatch(fetchAddress());
+                }}
+              >
+                Get Location
+              </Button>
+            </span>
+          )}
         </div>
 
         <div className="mb-12 flex items-center gap-5">
@@ -93,7 +120,16 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <Button disabled={isSubmitting} type="primary">
+          <input
+            type="hidden"
+            name="position"
+            value={`${position.latitude && position.longitude ? `${position.latitude},${position.longitude}` : ''}`}
+          />
+          <Button
+            disabled={isSubmitting || isLoadingAddress}
+            type="primary"
+            isLoading={isSubmitting}
+          >
             {isSubmitting
               ? 'Placing order...'
               : `Order now for ${formatCurrency(totalPrice)}`}
@@ -114,6 +150,8 @@ export async function action({ request }) {
     priority: data.priority === 'true',
   };
 
+  console.log('order', order);
+
   const errors = {};
   if (!isValidPhone(order.phone)) {
     errors.phone =
@@ -124,11 +162,13 @@ export async function action({ request }) {
 
   //? If there are no errors, create the order and redirect
 
-  const newOrder = await createOrder(order);
+  // const newOrder = await createOrder(order);
 
-  store.dispatch(clearCart());
+  // store.dispatch(clearCart());
 
-  return redirect(`/order/${newOrder.id}`);
+  // return redirect(`/order/${newOrder.id}`);
+
+  return null;
 }
 
 export default CreateOrder;
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -58,9 +58,10 @@ const userSlice = createSlice({
         state.position = action.payload.position;
         state.address = action.payload.address;
       })
-      .addCase(fetchAddress.rejected, (state, action) => {
+      .addCase(fetchAddress.rejected, (state) => {
         state.status = 'error';
-        state.error = action.error.message;
+        state.error =
+          'There was a problem fetching your address. Make sure to fill it manually.';
       }),
 });
 
